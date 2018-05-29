84aeadfad1423d39945134e6ffe997cabf3b048e
SHEFFmachine <sheff@sheffmachine.com>
2018-05-28 17:57:22 -0700

    Backend complete

---
+++ server/schema/types/user_type.js
@@ -0,0 +1,11 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const userType = new GraphQLObjectType({
+  name: 'UserType',
+  fields: {
+    email: { type: GraphQLString, }
+  }
+});
+
+module.exports = userType;
