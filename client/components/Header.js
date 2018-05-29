b646f70565bc5a47f802e3bf9c9f055cdd0b96a0
SHEFFmachine <sheff@sheffmachine.com>
2018-05-28 19:02:20 -0700

    Get login form set up, just need to wire everything up

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+
+import query from '../queries/CurrentUser';
+import mutation from '../mutations/LogOut';
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    });
+  }
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return <div></div>;
+    };
+
+    if (user) {
+      return (
+        <li><a onClick={this.onLogoutClick.bind(this)}>LogOut</a></li>
+      );
+    } else {
+      return (
+        <div>
+          <li><Link to="/signup">SignUp</Link></li>
+          <li><Link to="/login">LogIn</Link></li>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link className="brand-logo left" to="/">Home</Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
 
-export default Header;
+export default graphql(mutation)(
+  graphql(query)(Header)
+);
--- client/index.js
+++ client/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRouter } from 'react-router';
 
 import App from './components/App';
+import LoginForm from './components/LoginForm';
+
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
 
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject(o) {
     return o.id;
   }
@@ -16,8 +25,9 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path='/' component={App}/>
-        
+        <Route path='/' component={App}>
+          <Route path='/login' component={LoginForm} />
+        </Route>
       </Router>
     </ApolloProvider>
   );
